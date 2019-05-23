e3fd447fe4aed3a1e70689447974afd31800270a
ProgramadorViking <vikingdanel89@gmail.com>
2019-05-23 20:10:30 +0200

    Actualizado composer para phinx y cambio de nombre variable mal escrita

--- app/Users.php
+++ app/Users.php
@@ -1,7 +1,7 @@
 <?php
   class User extends Entidad {
 
-    private $user_id, $password, $worker_id, $name, $date1, $active, $role_id, $tken;
+    private $user_id, $password, $worker_id, $name, $date1, $active, $role_id, $token;
 
     public function __construct() {
       parent::__construct('users');
